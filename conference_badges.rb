1cca73d7a35e5a81fcf2285d5da9f736cdfe82ce
Jas Spencer <jas.spencer414@gmail.com>
2019-10-13 01:16:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
  "Hello, my name is #{name}."
  end
 
-def batch_badge_creator(speaker)
+def batch_badge_creator(speakers)
   speakers.map do |speaker|
     "Hello, my name is #{speaker}."
   end
